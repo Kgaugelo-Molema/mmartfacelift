3f8b18575a624e7ee112720a8038072ea798aa8c
Kgaugelo Molema <kmolema@gmail.com>
2019-09-21 11:43:05 +0200

    Disable DB connection

--- inc/dbconfiginc.php
+++ inc/dbconfiginc.php
@@ -3,8 +3,8 @@
     const DB_USER = "mmartqlz"; // set database user
     const DB_PASS = "MMArt@Afrihost123"; // set database password
     const DB_NAME = "mmartqlz_cms"; // set database name
-    $connection = new mysqli(DB_HOST,DB_USER,DB_PASS); 
+    //$connection = new mysqli(DB_HOST,DB_USER,DB_PASS); 
     /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    if (!$connection->select_db(DB_NAME)) {die ("lsdcdb selection failed<br>".$connection->error);}
+    //if (!$connection->select_db(DB_NAME)) {die ("lsdcdb selection failed<br>".$connection->error);}
 ?>
