bac619d6f1d82738717a9872e0b8f741ac52ecdc
Kgaugelo Molema <kmolema@gmail.com>
2020-04-10 20:14:53 +0200

    Robot

--- res/robotCheck.php
+++ res/robotCheck.php
@@ -1,11 +1,10 @@
-void method(Object arg) {
-    if(org  == null) {
-       
-    } else {
-       parameter here
+<?php
+function method($arg)
+{
+    if ($arg != null) {
+        // make a request to the url
+        // evaluate the result and check if it contains "./coming/"
+        // return true / false depending on the result
     }
 }
-
-void method() {
-    methode(null);
-}
+?>
